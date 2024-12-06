bdca9d305fe04a27a8f867ddc8d63b2516fed968
jonas-f-s <jonaasshotmail@gmail.com>
2024-12-06 17:18:03 -0300

    Removido componente e ajustadas rotas e módulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,10 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { TipoDeCadastroComponent } from './components/tipo-de-cadastro/tipo-de-cadastro.component';
 import { CadastroIndividualComponent } from './components/cadastro-individual/cadastro-individual.component';
-import { DadosSocioeconomicosComponent } from './components/dados-socioeconomicos/dados-socioeconomicos.component';
+import { CadastroDomiciliarTerritorialComponent } from './components/cadastro-domiciliar-territorial/cadastro-domiciliar-territorial.component';
+import { StatusDeSincronizacaoComponent } from './components/status-de-sincronizacao/status-de-sincronizacao.component';
+import { PacientesRecentesComponent } from './components/pacientes-recentes/pacientes-recentes.component';
+import { VisitasCadastradasComponent } from './components/visitas-cadastradas/visitas-cadastradas.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
@@ -14,7 +17,10 @@ const routes: Routes = [
   {path: 'about', component: AboutComponent},
   {path: 'tipo-de-cadastro', component: TipoDeCadastroComponent},
   {path: 'cadastro-individual', component: CadastroIndividualComponent},
-  {path: 'dados-socioeconomicos', component: DadosSocioeconomicosComponent},
+  {path: 'cadastro-domiciliar-territorial', component: CadastroDomiciliarTerritorialComponent},
+  {path: 'status-de-sincronizacao', component: StatusDeSincronizacaoComponent},
+  {path: 'pacientes-recentes', component: PacientesRecentesComponent},
+  {path: 'visitas-cadastradas', component: VisitasCadastradasComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,10 @@ import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TipoDeCadastroComponent } from './components/tipo-de-cadastro/tipo-de-cadastro.component';
 import { CadastroIndividualComponent } from './components/cadastro-individual/cadastro-individual.component';
-import { DadosSocioeconomicosComponent } from './components/dados-socioeconomicos/dados-socioeconomicos.component';
+import { CadastroDomiciliarTerritorialComponent } from './components/cadastro-domiciliar-territorial/cadastro-domiciliar-territorial.component';
+import { StatusDeSincronizacaoComponent } from './components/status-de-sincronizacao/status-de-sincronizacao.component';
+import { PacientesRecentesComponent } from './components/pacientes-recentes/pacientes-recentes.component';
+import { VisitasCadastradasComponent } from './components/visitas-cadastradas/visitas-cadastradas.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { DadosSocioeconomicosComponent } from './components/dados-socioeconomico
     FooterComponent,
     TipoDeCadastroComponent,
     CadastroIndividualComponent,
-    DadosSocioeconomicosComponent
+    CadastroDomiciliarTerritorialComponent,
+    StatusDeSincronizacaoComponent,
+    PacientesRecentesComponent,
+    VisitasCadastradasComponent
   ],
   imports: [
     BrowserModule,
